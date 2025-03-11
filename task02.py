9b3b19a5578f3fa2185ca573edfe0c4483331877
Questceque <86818261+Questceque@users.noreply.github.com>
2025-03-11 20:10:16 +1000

    Update task02.py

--- task02.py
+++ task02.py
@@ -8,18 +8,17 @@ We guarantee, that the given sequence contain >= 0 integers inside.
 """
 from collections import Sequence
 
-def check_fibonacci(data: Sequence[int]) -> bool:
-    if len(data) < 2:
+def check_fib(sequence: Sequence[int]) -> bool:
+    if len(sequence) < 2:
         return False
-    for i in range(2, len(data)):
-        if data[i] != data[i - 1] + data[i - 2]:
+   
+    if sequence[0] != 0 or sequence[1] != 1:
+        return False
+        
+    if len(sequence) == 2:
+        return True
+    
+    for i in range(2, len(sequence)):
+        if sequence[i] != sequence[i-1] + sequence[i-2]:
             return False
-            
-    return True
-
-
-print(check_fibonacci([0, 1, 1, 2, 3, 5, 8]))  # Output: True
-print(check_fibonacci([1, 1, 2, 3, 5, 8, 13]))  # Output: True
-print(check_fibonacci([1, 2, 3, 5, 8, 12]))      # Output: False
-print(check_fibonacci([0]))                       # Output: False
-       
+    
